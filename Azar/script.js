14fa4965b24c0b4e11a76c3a1e7c85dbf4d1f5a3
mai <mai@mai>
2017-10-29 19:59:55 +0100

    Creada función dado

---
+++ Azar/script.js
@@ -0,0 +1,17 @@
+function dado(caras = 0, veces = 1) {
+
+    if (/^[0-9]+$/.test(caras)) {
+
+        if (veces == 1) {
+            return Math.ceil(Math.random() * caras);
+        } else {
+            var arr;
+            for (let i = 0; i < veces; i++) {
+                arr[i] = Math.ceil(Math.random() * caras);
+            }
+            return arr;
+        }
+
+    }
+
+}
