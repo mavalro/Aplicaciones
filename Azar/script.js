2d949999d75d70dae1bf65f1e0b69910c23b14ba
mai <mai@mai>
2017-10-29 21:12:47 +0100

    Modificaciones

--- Azar/script.js
+++ Azar/script.js
@@ -2,16 +2,18 @@ function dado(caras = 0, veces = 1) {
 
     if (/^[0-9]+$/.test(caras)) {
 
-        if (veces == 1) {
-            return Math.ceil(Math.random() * caras);
-        } else {
-            let arr = [];
-            for (let i = 0; i < veces; i++) {
-                arr[i] = Math.ceil(Math.random() * caras);
-            }
-            return arr;
-        }
 
+        let arr = [];
+        let result = '<table><tr><th>Nº Tirada</th><th>Resultado</th></tr>'
+
+        for (let i = 0; i < veces; i++) {
+            arr[i] = Math.ceil(Math.random() * caras);
+
+            result += `<tr><td>${i+1}</td><td>${Math.ceil(Math.random() * caras)}</td></tr>`;
+        }
+        result += "</table>"
+
+        return result;
     }
 
 }
