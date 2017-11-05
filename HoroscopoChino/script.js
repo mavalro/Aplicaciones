5e06d7ecc1e4ca29e180f6efc99efe401099fa52
mavalro <mavalrodev@gmail.com>
2017-11-05 02:56:27 +0100

    Código mejorado

--- HoroscopoChino/script.js
+++ HoroscopoChino/script.js
@@ -6,9 +6,22 @@ function calcular(anyo)
     if ( !/^[0-9]+$/.test(anyo) ) {
         signo = 'Año no válido, introduce uno diferente.'
     } else {
-        signo='calculando...';
+
+        anyo = reducir(anyo);
+        for (let i in signos) {
+            if (anyo == i) {
+                signo = signos[i];
+            }
+        }
     }
 
 
     resultado.innerHTML = signo;
+}
+
+function reducir(input) {
+    while (!/^([0-9]|([1][0-1]))$/.test(input)) {
+        input -= 12;
+    }
+    return input;
 }
