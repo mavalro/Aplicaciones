a1f4d050386500f7786cefb994ea77405457f84e
mavalro <mavalrodev@gmail.com>
2017-11-05 03:19:40 +0100

    Añadiendo elemento

--- HoroscopoChino/script.js
+++ HoroscopoChino/script.js
@@ -1,12 +1,13 @@
 var signos = ['Mono', 'Gallo', 'Perro', 'Jabalí', 'Rata', 'Buey', 'Tigre', 'Conejo', 'Dragón', 'Serpiente', 'Caballo', 'Cabra'];
 
+var elementos = ['metal', 'agua', 'madera', 'fuego', 'tierra'];
+
 function calcular(anyo)
 {
-    var signo;
-    if ( !/^[0-9]+$/.test(anyo) ) {
+    var signo, elemento = anyo;
+    if ( !/^[0-9]{1,5}$/.test(anyo) ) {
         signo = 'Año no válido, introduce uno diferente.'
     } else {
-
         anyo = reducir(anyo);
         for (let i in signos) {
             if (anyo == i) {
@@ -15,13 +16,18 @@ function calcular(anyo)
         }
     }
 
+    alert(elemento[elemento.length-1]);
 
     resultado.innerHTML = signo;
 }
 
-function reducir(input) {
-    while (!/^([0-9]|([1][0-1]))$/.test(input)) {
-        input -= 12;
+function reducir(anyo) {
+    while (!/^([0-9]|([1][0-1]))$/.test(anyo)) {
+        anyo -= 12;
     }
-    return input;
+    return anyo;
+}
+
+function elemento(anyo) {
+    anyo = anyo[anyo.length-1];
 }
